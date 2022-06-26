26a4fd93a94b063ce7f9bce18ce3f265f99c4044
PedroDeBos <pedrodebos@gmail.com>
2022-06-26 20:40:16 +0200

    Started with the PointCounter

---
+++ DeadByDaylight/R/PointCounter.R
@@ -0,0 +1,14 @@
+points<-list()
+
+pointCounter1<-function(hook1, hook2, hook3){
+  hook1*1+hook2*1+hook3*2
+}
+
+pointCounter1(4, 4, 4) #gives proper output
+
+
+
+
+pointCounter<-function(hook1, hook2, hook3, gens, escaped, bleedout, hatch){
+  
+}
